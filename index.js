20642ae8e9f12c5582f6b7b6f47f164bef74ef08
Phạm Nguyễn Anh Kiệt <133972870+j4sonph20@users.noreply.github.com>
2023-07-04 18:41:26 +0700

    kiet lac

--- index.js
+++ index.js
@@ -106,6 +106,20 @@ function calculateTotalSalary() {
   document.getElementById("total-salary").value = totalSalary;
   document.getElementById("employee-type").value = employeeType;
 }
+// Xóa nhân viên
+function deleteEmployee() {
+  var employeeId = document.getElementById("employee-id").value;
+}
+
+// Cập nhật nhân viên
+function updateEmployee() {
+  var employeeId = document.getElementById("update-account").value;
+}
+function searchEmployees() {
+  t
+  var employeeType = document.getElementById("employee-type-select").value;
+}
+
 
 function isEmail(email) {
   
@@ -137,3 +151,106 @@ function isValidPassword(password) {
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
   return passwordRegex.test(password);
 }
+
+
+// xóa nhân viên 
+function clearInputs() {
+  document.getElementById("account").value = "";
+  document.getElementById("fullname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("password").value = "";
+  document.getElementById("start-date").value = "";
+  document.getElementById("salary").value = "";
+  document.getElementById("position").value = "";
+  document.getElementById("hours-worked").value = "";
+}
+
+
+//thêm nhân viên
+let employees = [];
+
+// list add nhân viên
+function addEmployee() {
+  // Lấy giá trị từ các input
+  let account = document.getElementById("account").value;
+  let fullname = document.getElementById("fullname").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let startDate = document.getElementById("start-date").value;
+  let salary = document.getElementById("salary").value;
+  let position = document.getElementById("position").value;
+  let hoursWorked = document.getElementById("hours-worked").value;
+}
+  // Tạo đối tượng nhân viên
+//   let employee = {
+//     account: account,
+//     fullname: fullname,
+//     email: email,
+//     password: password,
+//     startDate: startDate,
+//     salary: salary,
+//     position: position,
+//     hoursWorked: hoursWorked
+//   };
+
+//   // Thêm nhân viên vào mảng
+//   employees.push(employee);
+
+//   // Hiển thị danh sách nhân viên
+//   displayEmployees();
+// }
+
+// function displayEmployees() {
+//   let employeeList = document.getElementById("employee-list");
+//   employeeList.innerHTML = "";
+
+//   // Duyệt qua danh sách nhân viên và hiển thị thông tin
+//   for (let i = 0; i < employees.length; i++) {
+//     let employee = employees[i];
+
+//     // Tạo một div để hiển thị thông tin nhân viên
+//     let employeeDiv = document.createElement("div");
+//     employeeDiv.innerHTML = `
+//       <p><strong>Tài khoản:</strong> ${employee.account}</p>
+//       <p><strong>Họ tên:</strong> ${employee.fullname}</p>
+//       <p><strong>Email:</strong> ${employee.email}</p>
+//       <p><strong>Mật khẩu:</strong> ${employee.password}</p>
+//       <p><strong>Ngày làm:</strong> ${employee.startDate}</p>
+//       <p><strong>Lương cơ bản:</strong> ${employee.salary}</p>
+//       <p><strong>Chức vụ:</strong> ${employee.position}</p>
+//       <p><strong>Giờ làm trong tháng:</strong> ${employee.hoursWorked}</p>
+//     `;
+
+//     employeeList.appendChild(employeeDiv);
+//   }
+// }
+// function calculateTotalSalary() {
+//   // Lấy giá trị từ các input
+//   let hoursWorked = parseInt(document.getElementById("hours-worked").value);
+
+//   // Tính tổng lương
+//   // ...
+
+//   // Đánh giá nhân viên dựa trên số giờ làm
+//   let evaluation = "";
+//   if (hoursWorked >= 192) {
+//     evaluation = "Nhân viên xuất sắc";
+//     document.getElementById("evaluation").className = "excellent";
+//   } else if (hoursWorked >= 176) {
+//     evaluation = "Nhân viên giỏi";
+//     document.getElementById("evaluation").className = "good";
+//   } else if (hoursWorked >= 160) {
+//     evaluation = "Nhân viên khá";
+//     document.getElementById("evaluation").className = "fair";
+//   } else {
+//     evaluation = "Nhân viên trung bình";
+//     document.getElementById("evaluation").className = "average";
+//   }
+
+//   // Hiển thị đánh giá
+//   document.getElementById("evaluation").textContent = evaluation;
+
+//   // Hiển thị danh sách nhân viên
+//   // ...
+// }
+
